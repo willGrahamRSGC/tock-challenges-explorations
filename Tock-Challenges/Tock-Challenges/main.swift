96e7e97fa14124fae6691c2e26519ee12b72a53a
Russell Gordon <rgordon@rsgc.on.ca>
2016-01-28 09:57:13 -0500

    Initial setup for Tock group side challenges

---
+++ Tock-Challenges/Tock-Challenges/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tock-Challenges
+//
+//  Created by Russell Gordon on 2016-01-28.
+//  Copyright © 2016 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

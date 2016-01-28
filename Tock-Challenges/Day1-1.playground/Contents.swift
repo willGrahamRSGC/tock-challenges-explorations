3831abf09af9fecf8b21086230cca18aff825dc6
Will <student@MacBook-Pro-0690-Graham.local>
2016-01-28 10:31:56 -0500

    i don't understand how to make an input bar other than just manualy inputting the numbers.

--- Tock-Challenges/Day1-1.playground/Contents.swift
+++ Tock-Challenges/Day1-1.playground/Contents.swift
@@ -3,3 +3,34 @@
 import Cocoa
 
 var str = "Hello, playground"
+print("please enter month")
+var month = Int(readLine(stripNewline: true)!)!
+print("please enter day")
+var day = Int(readLine(stripNewline: true)!)!
+let special = 18
+
+// create conditionals
+if (month < 2) {
+  print("before")
+}
+
+if (month > 2){
+print ("after")
+}
+if (month == 2) {
+    if(day < 18) {
+    print ("before")
+    }
+}
+
+if (month == 2) {
+    if(day > 18) {
+    print("after")
+    }
+}
+
+if (month == 2) {
+    if (day == 18){
+    print("special")
+    }
+}
